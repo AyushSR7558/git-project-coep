a84424a1c2c4c310f70447581e0aa5cfa7bfec4b
Sharvari Sawarkar <sharvarisawarkar220706@gmail.com>
2026-04-16 15:14:04 +0530

    Rest all methods added to User class

--- src/User.java
+++ src/User.java
@@ -33,5 +33,51 @@ public class User {
         System.out.println("Registration Successful!");
         return accountNumber;
     }
+    // Display all accounts of the user
+    void displayAccounts() {
+        if (bankAccounts.isEmpty()) {
+            System.out.println("No bank accounts found!");
+            return;
+        }
 
+        for (Map.Entry<Bank, Set<String>> entry : bankAccounts.entrySet()) {
+            System.out.println("Bank: " + entry.getKey());
+            for (String acc : entry.getValue()) {
+                System.out.println("  Account No: " + acc);
+            }
+        }
+    }
+
+    // Check if user has account in a bank
+    boolean hasBank(Bank bank) {
+        return bankAccounts.containsKey(bank);
+    }
+
+    // Deposit money
+    void deposit(Bank bank, String accountNumber, int amount) {
+        if (!hasBank(bank)) {
+            System.out.println("User not registered in this bank!");
+            return;
+        }
+
+        if (bank.deposit(accountNumber, amount)) {
+            System.out.println("Deposit Successful!");
+        } else {
+            System.out.println("Deposit Failed!");
+        }
+    }
+
+    // Withdraw money
+    void withdraw(Bank bank, String accountNumber, int amount) {
+        if (!hasBank(bank)) {
+            System.out.println("User not registered in this bank!");
+            return;
+        }
+
+        if (bank.withdraw(accountNumber, amount)) {
+            System.out.println("Withdrawal Successful!");
+        } else {
+            System.out.println("Withdrawal Failed!");
+        }
+    }
 }
