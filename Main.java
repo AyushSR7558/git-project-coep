ce980c775331ee5fb272adf96a3f7c77660d8e8a
Ayush <ayushraut7558@gmail.com>
2026-04-16 19:34:18 +0530

    add/ handle each user

--- Main.java
+++ Main.java
@@ -93,5 +93,113 @@ public class Main {
             }
         } while (true);
     }
-		
+    static void handleEachUser(User user) {
+        int choice;
+        do {
+            System.out.println("\n--- User: " + user.name + " ---");
+            System.out.println("1) Register to a Bank");
+            System.out.println("2) Display All Accounts");
+            System.out.println("3) Deposit Money");
+            System.out.println("4) Withdraw Money");
+            System.out.println("5) Check Balance");
+            System.out.println("6) Back");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    if (allBank.isEmpty()) {
+                        System.out.println("No banks available for registration!");
+                        break;
+                    }
+
+                    System.out.println("Available Banks:");
+                    for (int i = 0; i < allBank.size(); i++) {
+                        System.out.println(i + ") " + allBank.get(i).name);
+                    }
+                    System.out.print("Select bank: ");
+                    int bankChoice = sc.nextInt();
+                    sc.nextLine();
+
+                    if (bankChoice >= 0 && bankChoice < allBank.size()) {
+                        String accountNumber = user.registerToBank(allBank.get(bankChoice));
+                        System.out.println("Your new account number: " + accountNumber);
+                    } else {
+                        System.out.println("Invalid bank selection!");
+                    }
+                    break;
+
+                case 2:
+                    user.displayAccounts();
+                    break;
+
+                case 3:
+                    if (allBank.isEmpty()) {
+                        System.out.println("No banks available!");
+                        break;
+                    }
+
+                    System.out.println("Select Bank:");
+                    for (int i = 0; i < allBank.size(); i++) {
+                        System.out.println(i + ") " + allBank.get(i).name);
+                    }
+                    System.out.print("Enter bank number: ");
+                    int depositBank = sc.nextInt();
+                    sc.nextLine();
+
+                    if (depositBank >= 0 && depositBank < allBank.size()) {
+                        Bank selectedBank = allBank.get(depositBank);
+                        System.out.print("Enter account number: ");
+                        String accNum = sc.nextLine();
+                        System.out.print("Enter amount to deposit: ");
+                        int depositAmount = sc.nextInt();
+                        sc.nextLine();
+
+                        user.deposit(selectedBank, accNum, depositAmount);
+                    } else {
+                        System.out.println("Invalid bank selection!");
+                    }
+                    break;
+
+                case 4:
+                    if (allBank.isEmpty()) {
+                        System.out.println("No banks available!");
+                        break;
+                    }
+
+                    System.out.println("Select Bank:");
+                    for (int i = 0; i < allBank.size(); i++) {
+                        System.out.println(i + ") " + allBank.get(i).name);
+                    }
+                    System.out.print("Enter bank number: ");
+                    int withdrawBank = sc.nextInt();
+                    sc.nextLine();
+
+                    if (withdrawBank >= 0 && withdrawBank < allBank.size()) {
+                        Bank selectedBank = allBank.get(withdrawBank);
+                        System.out.print("Enter account number: ");
+                        String accNum = sc.nextLine();
+                        System.out.print("Enter amount to withdraw: ");
+                        int withdrawAmount = sc.nextInt();
+                        sc.nextLine();
+
+                        user.withdraw(selectedBank, accNum, withdrawAmount);
+                    } else {
+                        System.out.println("Invalid bank selection!");
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Feature to be implemented: Check Balance");
+                    break;
+
+                case 6:
+                    return;
+
+                default:
+                    System.out.println("Wrong Input, Try Again!!!");
+            }
+        } while (true);
+    }		
 }
